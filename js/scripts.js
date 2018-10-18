b5db6d1ec3c1e6849821c5e4028f65a948b726e3
Katrina Hockman and Robert Lee <robert@>
2018-10-18 15:13:29 -0700

    Add logic for equi, isos, and scal triangles.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#triangle").submit(function(event) {
+    var sideA = $("input#side1").val();
+    var sideB = $("input#side2").val();
+    var sideC = $("input#side3").val();
+
+    if ((sideA + sideB < sideC) && (sideA + sideC < sideB) && (sideB + sideC < sideA)) {
+        $("#none").show();
+
+    } else if (sideA === sideB && sideB === sideC && sideA === sideC) {
+        $("#equilateral").show();
+
+    } else if ((sideA === sideB) || (sideA === sideC) || (sideB === sideC)) {
+       $("#isosceles").show();
+
+    } else {
+      $("#scalene").show();
+
+
+
+    }
+  event.preventDefault();
+  });
+
+});
